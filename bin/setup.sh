5f72982f3899ae5ed5785fe00d5f558a65ee80d7
Ephram Cola Jacquin <ecolajacquin@gmail.com>
2024-02-20 19:33:00 -0500

    feat: edit conflict in setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-<<<<<<< HEAD
 # download develop branch from remote repository
 git checkout develop
 
-=======
->>>>>>> feature/our-symbol-library
 # initialize git flow
 git flow init -d
 
